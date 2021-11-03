09b06a87464d2ba924b83118e1a866e854fd983e
trinetry <stro2007@ukr.net>
2021-11-03 10:48:23 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,5 +7,19 @@ const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
 player.setCurrentTime(currentTime);
 player.on('timeupdate', throttle(data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds), 1000));
